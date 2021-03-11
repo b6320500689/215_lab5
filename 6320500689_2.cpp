0d4ba6fdbdb04d23b858dc69381cb1a835a92372
b6320500689 <supitchaya.r@ku.th>
2021-03-11 15:07:23 +0700

    2222

--- 6320500689_2.cpp
+++ 6320500689_2.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-	int n=1,x=0,y;
+	int n=1,x=0,y,i,j;
 	if (n<=1000 && n>=1)
 		scanf("%d",&n);
 	if (n%2 == 0)
@@ -14,6 +14,17 @@ int main ()
 	{
 		y = ((n+x)/2);
 	}
-	printf ("%d",y);
+	
+	for(i=0; i<(n+x)/2; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			if(j == (n/2)-i || j == (n/2)+i)
+				printf ("*");
+			else
+				printf ("_");
+		}
+		printf("\n");
+	}
 	return 0;
 }
