9e626ae2ca79a816d289d91e9291ac9d438b588c
b6320500689 <supitchaya.r@ku.th>
2021-03-11 15:41:32 +0700

    33333

--- 6320500689_3.cpp
+++ 6320500689_3.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main ()
 {
-	int n,m;
-	scanf ("%d",&n);
-	
-	printf ("%d",m);
+	int n[11] = {1,2,3,4,5,6,8,9,10,12,15};
+	int m,i;
+	scanf ("%d",&m);
+	for (i=0; i<m; i++)
+	printf ("%d",n[i]);
 	return 0;
 }
