d5068ca0adcadd3ad9732f3f4d680bc1d6e08d95
b6320500689 <supitchaya.r@ku.th>
2021-03-11 15:04:15 +0700

    22

--- 6320500689_2.cpp
+++ 6320500689_2.cpp
@@ -1,9 +1,19 @@
 #include <stdio.h>
 int main ()
 {
-	int n=1;
-	if(n<=1000 && n>=1)
+	int n=1,x,y;
+	if (n<=1000 && n>=1)
 		scanf("%d",&n);
-		
+	if (n%2 == 0)
+	{
+  		n--;
+  		x++;
+  		y = ((n+x)/2)-1;
+	}
+	else
+	{
+		y = ((n+x)/2);
+	}
+	printf ("%d",y);
 	return 0;
 }
